cd21ec573e5ece49b4bd5021ff0bf2a81978cc3f
Kyle <37756806+kjm1550@users.noreply.github.com>
2018-12-12 22:48:19 -0600

    Added the Roman Numberal Converter
    Added the Roman Numeral Converter project and made some style changes to the homepage.

--- js/script-js.js
+++ js/script-js.js
@@ -14,4 +14,24 @@ $(document).ready(() => {
 			document.getElementById("pc_output").innerHTML = "Not A Palidrone";
 		}
 	});
+
+
+	const realNums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+	const romanNums = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+	$("#rnc_submit").click(function() {
+		var num = document.getElementById("rnc_input").value;
+		var romanized = '';
+		for (x = 0; x < realNums.length; x++) {
+			while (realNums[x] <= num) {
+				if (num >= realNums[x]) {
+					romanized += romanNums[x];
+					num -= realNums[x];
+				}
+			}
+		}
+		document.getElementById("rnc_output").innerHTML = romanized;
+	});
+
+	
 });
